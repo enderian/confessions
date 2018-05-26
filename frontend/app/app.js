3daa85af4cefa9bbd7424ccba7b2d908839e08e2
Enderian <spyrospagk@gmail.com>
2018-05-26 13:06:26 +0300

    Removed font awesome from project

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -5,16 +5,3 @@ import 'bootstrap'
 
 import './form'
 import './index'
-
-import fontawesome from '@fortawesome/fontawesome'
-import faSearch from '@fortawesome/fontawesome-pro-solid/faSearch'
-import faUpload from '@fortawesome/fontawesome-pro-solid/faUpload'
-import faTimes from '@fortawesome/fontawesome-pro-regular/faTimes'
-import faCopyright from '@fortawesome/fontawesome-pro-solid/faCopyright'
-import faPaperPlane from '@fortawesome/fontawesome-pro-solid/faPaperPlane'
-import faCheck from '@fortawesome/fontawesome-pro-solid/faCheck'
-import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebook'
-import faEnvelope from '@fortawesome/fontawesome-pro-solid/faEnvelope'
-import faSignIn from '@fortawesome/fontawesome-pro-solid/faSignIn'
-
-fontawesome.library.add(faSearch, faUpload, faTimes, faCopyright, faPaperPlane, faCheck, faFacebook, faEnvelope, faSignIn);
