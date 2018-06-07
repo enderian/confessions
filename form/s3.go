8db3acadac0ca5278c18c88ff2d3cea0640f33a0
enderian <spyrospagk@gmail.com>
2018-06-07 14:51:38 +0300

    Corrected bucket to upload to.

--- form/s3.go
+++ form/s3.go
@@ -3,11 +3,14 @@ package form
 import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
 var s3uploader *s3manager.Uploader
 
 func SetupS3() {
-	s := session.Must(session.NewSession())
+	s := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("eu-central-1"),
+	}))
 	s3uploader = s3manager.NewUploader(s)
 }
